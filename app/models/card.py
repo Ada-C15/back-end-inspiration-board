d12969d3a8c2a1d40aa243e2cf8e505e38ff67c6
Hannah Lumapas <lumapas.h@gmail.com>
2021-06-29 14:54:24 -0400

    sets up outline for routes

--- app/models/card.py
+++ app/models/card.py
@@ -5,3 +5,5 @@ class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String)
     likes_count = db.Column(db.Integer)
+    # set default like count to 0 
+    # autoincrement = True? -> it should go up by 1, could do this in the routes? 
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,34 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
 
+from app.models.board import Board
+
 # example_bp = Blueprint('example_bp', __name__)
+boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
+
+
+#=====================================================#
+#                   BOARD ROUTES                      #
+#=====================================================#
+
+# (create) POST a new board
+@boards_bp.route("",)
+
+# (read) GET all the boards
+
+# (read) GET a board by id 
+
+#=====================================================#
+#                    CARD ROUTES                      #
+#=====================================================#
+
+
+# (create) POST a new card for a selected board by id 
+@boards_bp.route("",)
+
+# (read) GET all the cards for a selected board 
+
+# DELETE a card 
+
+
+# +1 likes feature, part of card model 

415df019832385ea1a3e5f718c428e76e50091de
Daniela Sanchez <dani.sanchez077@gmail.com>
2021-06-29 14:29:20 -0700

    updated card model with board id FK, added new boards/<board_id>/cards POST route

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
 from .models.board import Board
+from .models.card import Card
+
 
 board_bp = Blueprint("boards", __name__, url_prefix="/boards")
+card_bp = Blueprint("cards", __name__, url_prefix="/cards")
 
 @board_bp.route("", methods=["POST"], strict_slashes=False)
 def create_board():
@@ -26,3 +29,25 @@ def get_boards():
     boards = Board.query.all()
     boards_response = [board.to_dict() for board in boards]
     return make_response(jsonify(boards_response), 200)
+
+
+@board_bp.route("/<board_id>/cards", methods=["POST"], strict_slashes=False)
+def add_card_to_board(board_id):
+    request_body=request.get_json()
+    
+    if "message" in request_body and "likes_count" in request_body:
+        new_card = Card(message= request_body["message"],
+                            likes_count= request_body["likes_count"],
+                            board_id= int(board_id))
+        db.session.add(new_card)
+        db.session.commit()    
+
+        return make_response({"card": new_card.to_dict()},201)   
+
+    elif "message" not in request_body:
+        return make_response({"details": "Message data invalid"}, 400)
+    
+    elif "likes_count" not in request_body:
+        return make_response({"details": "Likes_count data invalid"}, 400)
+    # likes_count might need to be refactors to default to zero when new card is created!!
+
--- migrations/versions/e0c65ce58412_adds_board_and_card_models.py
+++ migrations/versions/e0c65ce58412_adds_board_and_card_models.py
@@ -1,8 +1,8 @@
 """adds Board and Card models
 
-Revision ID: 84140d7d4f88
+Revision ID: e0c65ce58412
 Revises: 
-Create Date: 2021-06-29 09:49:13.108519
+Create Date: 2021-06-29 14:06:19.739580
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '84140d7d4f88'
+revision = 'e0c65ce58412'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -28,6 +28,8 @@ def upgrade():
     sa.Column('card_id', sa.Integer(), autoincrement=True, nullable=False),
     sa.Column('message', sa.String(), nullable=True),
     sa.Column('likes_count', sa.Integer(), nullable=True),
+    sa.Column('board_id', sa.Integer(), nullable=True),
+    sa.ForeignKeyConstraint(['board_id'], ['board.board_id'], ),
     sa.PrimaryKeyConstraint('card_id')
     )
     # ### end Alembic commands ###

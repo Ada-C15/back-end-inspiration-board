fb86aae02196edc10154682d22019211f4922d14
Erika Chikere <erikachik08@gmail.com>
2021-06-28 19:18:50 -0700

    Backend endpoints,routes,blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -32,11 +32,11 @@ def create_app():
     # from .routes import example_bp
     # app.register_blueprint(example_bp)
 
-    # from .routes import task_list_bp
-    # app.register_blueprint(task_list_bp)
+    from .routes import board_bp
+    app.register_blueprint(board_bp)
 
-    # from .routes import goals_bp
-    # app.register_blueprint(goals_bp)
+    from .routes import card_bp
+    app.register_blueprint(card_bp)
     
 
     CORS(app)
--- app/models/board.py
+++ app/models/board.py
@@ -4,4 +4,18 @@ class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     owner = db.Column(db.String)
-   
+    cards = db.relationship('Card', backref='board', lazy=True)
+
+
+    @classmethod
+    def from_json(cls, board_dict): 
+        return Board (title = board_dict["title"], 
+                owner = board_dict["owner"])
+
+    def as_json(self): 
+        return {
+            "board_id": self.board_id,
+            "title": self.title,
+            "owner": self.owner,
+
+        }

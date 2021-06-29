065edbe5f12c9f66c0c9be7637c9a59d308b79dc
SterlingSunshine <katrina.kimzey@gmail.com>
2021-06-28 17:28:30 -0700

    Started set up, flask init not finished

--- app/models/board.py
+++ app/models/board.py
@@ -1,1 +1,19 @@
+'''
+  Board Model - 
+    - constructor
+    - to_dict which formats info into a dict for response
+'''
 from app import db
+
+class Board(db.Model):
+    board_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    owner = db.Column(db.String)
+    cards = db.relationship('Card', backref='board', lazy=True)
+
+    def to_dict(self):
+        return{
+            "id" : self.board_id,
+            "title" : self.title,
+            "owner" : self.owner,
+        }
--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,18 @@
+'''
+  Card Model - 
+    - constructor
+    - to_dict which formats info into a dict for response
+'''
 from app import db
+
+class Card(db.Model):
+  card_id = db.Column(db.Integer, primary_key=True)
+  message = db.Column(db.String)
+  likes_count = db.Column(db.Integer)
+
+  def to_dict(self):
+    return {
+      "id" : self.card_id,
+      "message" : self.message,
+      "likes" : self.likes_count,
+    }

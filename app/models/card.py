b1de1cbdef2e89ec2358d32bdb4195797ea057f9
Libby L. Gerber <libby@libbygerber.com>
2021-06-29 16:53:32 -0700

    added DELETE and PUT endpoints for Card

--- app/models/card.py
+++ app/models/card.py
@@ -8,3 +8,10 @@ class Card(db.Model):
     likes_count = db.Column(db.Integer, default=0)
     board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'), nullable=True)
 
+    def card_json(self):
+        return {
+            "card_id": self.card_id,
+            "message": self.message,
+            "likes_count": self.likes_count,
+            "board_id": self.board_id
+        }

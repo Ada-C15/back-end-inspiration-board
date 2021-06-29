d1020aa850e01e1ff54aa090602c867f9c1b062b
Stella Shi <shiyu.stella.shi@gmail.com>
2021-06-29 15:34:59 -0400

    cards endpoints and CRUD done.

--- app/routes.py
+++ app/routes.py
@@ -20,11 +20,11 @@ def handle_boards():
             db.session.add(new_board)
             db.session.commit()
 
-            response = {"board":new_board.board_response()}
+            response = new_board.board_response()
             return jsonify(response), 201
 
         else:
-            return make_response ({"Invalid data error": "please include title and owner information"},400)
+            return make_response ("Invalid data error: please include title and owner information",400)
 
     elif request.method == "GET":
         boards = Board.query.all()
@@ -33,3 +33,101 @@ def handle_boards():
         for board in boards:
             boards_response.append(board.board_response())
         return jsonify(boards_response),200
+
+
+@boards_bp.route("/<board_id>", methods=["GET","PUT","DELETE"])
+def handle_a_board(board_id):
+    board = Board.query.get(board_id)
+    if board is None:
+        return make_response ("Invadlid Board ID",404)
+
+    elif request.method == "GET":
+        response = board.board_response()
+        return jsonify(response), 200
+
+    elif request.method == "PUT":      
+        form_data = request.get_json()
+        board.title = form_data["title"]
+        board.owner = form_data["owner"]
+
+        db.session.commit()
+        
+        response = board.board_response()
+        return jsonify(response), 200
+        
+    elif request.method == "DELETE":
+        db.session.delete(board)
+        db.session.commit()
+        response = f"Board #{board.board_id} {board.title} successfully deleted"
+        return jsonify(response), 200
+
+
+## Cards ## 
+@cards_bp.route("", methods=["POST","GET"])
+def handle_cards():
+    if request.method == "POST":
+        request_body = request.get_json()
+        if 'message' in request_body: 
+            new_card = Card(message=request_body["message"])
+            db.session.add(new_card)
+            db.session.commit()
+
+            response = new_card.card_response()
+            return jsonify(response), 201
+
+        else:
+            return make_response ("Invalid data error: please include title and owner information",400)
+
+    elif request.method == "GET":
+        cards = Card.query.all()
+    
+        cards_response = []
+        for card in cards:
+            cards_response.append(card.card_response())
+        return jsonify(cards_response),200
+
+
+@cards_bp.route("/<card_id>", methods=["GET","PUT","DELETE"])
+def handle_a_card(card_id):
+    card = Card.query.get(card_id)
+    if card is None:
+        return make_response ("Invadlid Card ID",404)
+
+    elif request.method == "GET":
+        response = card.card_response()
+        return jsonify(response), 200
+
+    elif request.method == "PUT":      
+        form_data = request.get_json()
+        card.message = form_data["message"]
+        card.likes_count = form_data["likes_count"]
+
+        db.session.commit()
+        
+        response = card.card_response()
+        return jsonify(response), 200
+        
+    elif request.method == "DELETE":
+        db.session.delete(card)
+        db.session.commit()
+        response = f"Card #{card.card_id} successfully deleted"
+        return jsonify(response), 200
+
+
+@cards_bp.route("/<card_id>/like", methods=["PUT","GET"])
+def handle_a_card_like(card_id):
+    card = Card.query.get(card_id)
+    if card is None:
+        return make_response ("Invadlid Card ID",404)
+    
+    elif request.method == "GET":
+        response = card.card_response()
+        return jsonify(response["likes_count"]), 200
+
+    elif request.method == "PUT":      
+        form_data = request.get_json()
+        card.likes_count = form_data["likes_count"]
+
+        db.session.commit()
+        response = card.card_response()
+        return jsonify(response["likes_count"]), 200

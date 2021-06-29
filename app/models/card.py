40da7e6b331853a6ee6454efb617821177d5dcbc
Libby L. Gerber <libby@libbygerber.com>
2021-06-29 10:47:48 -0700

    added Card model

--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,8 @@
 from app import db
+from flask import current_app
+
+class Card(db.Model):
+    __tablename__="card"
+    card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    message = db.Column(db.String)
+    likes_count = db.Column(db.Integer)

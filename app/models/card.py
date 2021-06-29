a998023f104db72eb8a4710cfd738d1e9d27f29d
Hannah Lumapas <lumapas.h@gmail.com>
2021-06-29 16:22:29 -0400

    creates GET & POST routes for board

--- app/models/card.py
+++ app/models/card.py
@@ -3,7 +3,7 @@ from app import db
 
 class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    message = db.Column(db.String)
+    message = db.Column(db.String(40)) # make less than 40 characters 
     likes_count = db.Column(db.Integer)
     # set default like count to 0 
     # autoincrement = True? -> it should go up by 1, could do this in the routes? 

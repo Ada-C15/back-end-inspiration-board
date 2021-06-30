28d76077aaa88b2d7f2f8048dac4d13619293360
Nataliia Getlin <natali.getlin@gmail.com>
2021-06-29 17:29:37 -0700

    Add endpoint DELETE /cards/<card_id>

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,8 @@ boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
 cards_bp = Blueprint("cards", __name__, url_prefix="/cards")
 
 # Get all boards
+
+
 @boards_bp.route("", methods=["GET"])
 def get_all_boards():
     query = Board.query.order_by(Board.board_id.asc())
@@ -15,9 +17,11 @@ def get_all_boards():
     for board in query:
         boards_list.append(board.get_resp())
 
-    return jsonify(boards_list), 200 
+    return jsonify(boards_list), 200
 
 # Create a board
+
+
 @boards_bp.route("", methods=["POST"])
 def create_board():
     request_body = request.get_json()
@@ -27,23 +31,26 @@ def create_board():
     )
     db.session.add(new_board)
     db.session.commit()
-    return (jsonify(f"Posted new board {new_board.title}!"),201)
-
+    return (jsonify(f"Posted new board {new_board.title}!"), 201)
 
 
 '''
 Next: Create, Read, and Delete Cards
 '''
 # Gets all cards
+
+
 @cards_bp.route("", methods=["GET"])
 def get_all_card():
     query = Card.query.order_by(Card.card_id.asc())
     cards_list = []
     for card in query:
         cards_list.append(card.get_resp())
-    return jsonify(cards_list), 200 
+    return jsonify(cards_list), 200
 
 # Get all cards associated with board
+
+
 @cards_bp.route("/<board_id>", methods=["GET"])
 def get_card(board_id):
     board = Board.query.get(board_id)
@@ -52,9 +59,11 @@ def get_card(board_id):
         board_card = Card.query.get(card.card_id)
 
         cards_list.append(board_card.get_resp())
-    return (jsonify(cards_list),201)
+    return (jsonify(cards_list), 201)
 
 # Create a card for a board
+
+
 @cards_bp.route("/<board_id>", methods=["POST"])
 def create_card(board_id):
     request_body = request.get_json()
@@ -64,9 +73,20 @@ def create_card(board_id):
     )
     db.session.add(new_card)
     db.session.commit()
-    return (jsonify(f"Posted new card on board {new_card.board_id}!"),201)
+    return (jsonify(f"Posted new card on board {new_card.board_id}!"), 201)
 
-# # Delete a card 
-# @cards_bp.route("/<board_id>", methods=["DELETE"])
-# def delete_card(board_id):
-#     pass
+
+# # Delete a card
+# DELETE /cards/<card_id>
+@cards_bp.route("/<card_id>", methods=["DELETE"])
+def delete_card(card_id):
+    card = Card.query.get_or_404(
+        card_id,
+        description={
+            "error": True,
+            "error_message": f"Card with id {card_id} does not exist."})
+
+    db.session.delete(card)
+    db.session.commit()
+
+    return ({"details": f"Card {card_id} successfully deleted."}, 200)

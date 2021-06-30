636fc4b093ccfeaacf1733e1c296398487815a9a
Whit Sundby <wksundby@gmail.com>
2021-06-29 18:10:18 -0700

    adds tests for /boards endpoint

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,27 +1,128 @@
 from app.models.board import Board
+from app.models.card import Card
 
-def test_get_board():
+############################ TEST /BOARDS ENDPOINT ############################
+
+def test_get_all_boards_three_saved_boards(client, three_boards):
     #Act
+    response = client.get("/boards")
+    response_body = response.get_json()
+
     #Assert
-# 200 response code to get board and body with appropriate board (id, title, owner, cards?)
+    assert response.status_code == 200
+    assert response_body == [
+        {
+            "board_id": 1,
+            "title": "Pick me up",
+            "owner": "Simon"
+        },
+        {
+            "board_id": 2,
+            "title": "Cool plants",
+            "owner": "Jamie"
+        },
+        {
+            "board_id": 3,
+            "title": "Travel destinations",
+            "owner": "Alex"
+        }
+    ]
+
+def test_get_all_boards_no_saved_boards(client):
+    #Act
+    response = client.get("/boards")
+    response_body = response.get_json()
+
+    #Assert
+    assert response.status_code == 200
+    assert response_body == []
+
+def test_get_all_boards_one_saved_board(client, one_board):
+    #Act
+    response = client.get("/boards")
+    response_body = response.get_json()
+
+    #Assert
+    assert response.status_code == 200
+    assert response_body.length == 1
+    assert response_body == [
+        {
+            "board_id": 1,
+            "title": "Healthy Habits",
+            "owner": "Jose"
+        }
+    ]
+
+def test_create_board(client):
+    #Act
+    response = client.post("/boards", json={
+        "title": "New board!",
+        "owner": "Maite"
+    })
+    response_body = response.get_json()
+
+    #Assert
+    assert response.status_code == 201
+    assert "board" in response_body
+    assert response_body == {
+        "board": {
+            "board_id": 1,
+            "title": "New board!",
+            "owner": "Maite"
+        }
+    }
+
+def test_create_board_missing_title(client):
+    #Act
+    response = client.post("/boards", json={
+        "owner": "Johannes"
+    })
+    response_body = response.get_json()
+
+    #Assert
+    assert response.status_code == 400
+    assert response_body == {
+        "details": "Invalid data"
+    }
+
+def test_create_board_missing_owner(client):
+    #Act
+    response = client.post("/boards", json={
+        "title": "New board!",
+    })
+    response_body = response.get_json()
+
+    #Assert
+    assert response.status_code == 400
+    assert response_body == {
+        "details": "Invalid data"
+    }
 
 
+############################ TEST /BOARDS/{id} ENDPOINT ############################
 
+def test_get_board(client, one_board):
+    #Act
+    response = client.get("/boards/1")
+    response_body = response.get_json()
 
+    #Assert
+    assert response.status_code == 200
+    assert "board" in response_body
+    assert response_body == {
+        "board": {
+            "board_id": 1,
+            "title": "Healthy Habits",
+            "owner": "Jose"
+        }
+    }
 
-# def test_get_task(client, one_task):
-#     # Act
-#     response = client.get("/tasks/1")
-#     response_body = response.get_json()
+def test_get_board_not_found(client):
+    #Act
+    response = client.get("/boards/1")
+    response_body = response.get_json()
 
-#     # Assert
-#     assert response.status_code == 200
-#     assert "task" in response_body
-#     assert response_body == {
-#         "task": {
-#             "id": 1,
-#             "title": "Go on my daily walk 🏞",
-#             "description": "Notice something new every day",
-#             "is_complete": False
-#         }
-#     }
+    #Assert
+    assert response.status_code == 404
+    assert response_body == None
+

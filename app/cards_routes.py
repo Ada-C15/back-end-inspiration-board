55b594a0b3e1b7ba294e43da11e77a2765ed629f
kmalakhova <kgrekhova@gmail.com>
2021-06-29 20:17:28 -0700

    Add edit_card() and like_card()

--- app/cards_routes.py
+++ app/cards_routes.py
@@ -9,54 +9,65 @@ from flask import request, Blueprint, jsonify
 
 cards_bp = Blueprint("cards", __name__, url_prefix="/boards/board_id/cards")
 
-def get_card_response(card):
-    """Returns card's data"""
+def cards_data(card):
     return jsonify({"card": card.to_dict()})
 
-def get_error_response(card_id):
-    """Returns error message"""
+def card_not_found(card_id):
     return jsonify({"details": f"Card with id {card_id} is not found"})
+
+def message_valid(length):
+    if length > 40 or length <= 5:
+        return False
+    return True
+
+def not_valid_message():
+    return jsonify({"details": "Just try to refrase :)"})
 
 @cards_bp.route("", methods = ["POST"], strict_slashes=False)
 def add_card():
     """Adds a card to a board"""
     request_body = request.get_json() 
-    if len(request_body["message"]) > 40:
-        return jsonify({"details": f"Your message is too long"})
-    if len(request_body["message"]) == 0:
-        return jsonify({"details": f"You can't submit an empty card"})
+    length = len(request_body["message"])
+    if not message_valid(length):
+        return not_valid_message()
     card = Card(message = request_body["message"])
     db.session.add(card)
     db.session.commit()
-    return get_card_response(card)
+    return cards_data(card)
 
 @cards_bp.route("/<card_id>", methods=["DELETE"], strict_slashes=False)
 def delete_card(card_id):
     """Deletes a card"""
     card = Card.query.get(card_id)
     if not card:
-        return get_error_response(card_id)
+        return card_not_found(card_id)
     db.session.delete(card)
     db.session.commit()
     return ({"details": f"Card {card_id} \"{card.message}\" successfully deleted"})
 
-@cards_bp.route("/<card_id>", methods=["PATCH"], strict_slashes=False) 
-def update_card(card_id):
-    """Updates a portion of a single card"""
+@cards_bp.route("/<card_id>/like", methods=["PATCH"], strict_slashes=False) 
+def like_card(card_id):
+    """Upvotes a card"""
     card = Card.query.get(card_id)
     if not card:
-        return get_error_response(card_id)
-    request_body = request.get_json()
-    # Updates message
-    if request_body["message"]: #TODO update
-        card.message = request_body["message"]
-    # Updates likes count
-    else:
-        card.likes_count += 1
+        return card_not_found(card_id)
+    card.likes_count += 1
     db.session.commit()
-    return get_card_response(card)
+    return cards_data(card)
 
-
+@cards_bp.route("/<card_id>/edit", methods=["PATCH"], strict_slashes=False) 
+def edit_card(card_id):
+    """Updates message of a single card"""
+    card = Card.query.get(card_id)
+    if not card:
+        return card_not_found(card_id)
+    request_body = request.get_json()
+    length = len(request_body["message"])
+    if not message_valid(length):
+        return not_valid_message()
+    card.message = request_body["message"]
+    db.session.commit()
+    return cards_data(card)
 
 
 

502a6a9c81af791ebf8160cd4ab2255ea72542b3
Hannah Lumapas <lumapas.h@gmail.com>
2021-06-30 13:34:56 -0600

    adds POST a card to a board route, set the default likes_count, registers card blueprint

--- app/models/card.py
+++ app/models/card.py
@@ -5,9 +5,14 @@ from flask import current_app
 class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String)
-    likes_count = db.Column(db.Integer)
+    likes_count = db.Column(db.Integer, default=0)
     board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'), nullable=True)
 
-
+    def to_json(self): 
+        return {     
+            "message": self.message,
+            "likes_count": self.likes_count,
+            "board_id": self.board_id
+        }
     # set default like count to 0 
     # autoincrement = True? -> it should go up by 1, could do this in the routes? 

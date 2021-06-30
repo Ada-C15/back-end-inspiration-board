7d808e5f3719d7ed232468fc6af7835b1a265032
brew doctor <brew upgrade python@3.9>
2021-06-29 18:46:12 -0700

    joined Card and Board models.  created board_id/cards endpoints (get, post).  added card_id endpoint (delete).

--- app/models/board.py
+++ app/models/board.py
@@ -6,4 +6,5 @@ class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     owner = db.Column(db.String)
+    cards = db.relationship('Card', backref='board', lazy=True)
     
--- app/routes.py
+++ app/routes.py
@@ -22,3 +22,65 @@ def boards():
             })
             
         return jsonify(board_list)
+
+@boards_bp.route("<board_id>/cards", methods=["POST", "GET"], strict_slashes=False) # get all cards for specific board
+def handle_board_cards(board_id):
+
+    board = Board.query.get(board_id) # get correct board using id passed into endpoint
+
+    if board is None: # if board doesn't exist, return error
+        return "", 404
+
+    if request.method == "POST": # post card to one board
+
+        request_body = request.get_json() # deviated from Task List API logic here
+        
+        new_card = Card(message=request_body["message"],
+                        likes_count=request_body["likes_count"]) # this is going to have to become logic
+
+        new_card.board_id = board_id # assign board id to new instance of/row in Card
+
+        db.session.commit()
+
+        return {
+            "board_id": board.board_id,
+            "card_id": new_card.card_id # we might be returning something else here
+        }
+
+    if request.method == "GET": # get cards of one board
+
+        associated_cards = Card.query.filter_by(board_id=board_id)
+
+        list_of_cards = []
+
+        for card in associated_cards:
+            list_of_cards.append({
+                "card_id": card.card_id,
+                "board_id": card.board_id,
+                "message": card.message,
+                "likes_count": card.likes_count,
+        })
+
+        return {
+            "board_id": board.board_id, # board data was obtained on line 29
+            "board_title": board.title,
+            "cards": list_of_cards
+            }
+
+
+cards_bp = Blueprint("cards", __name__, url_prefix="/cards")
+
+@boards_bp.route("/<card_id>", methods=["DELETE"], strict_slashes=False)
+def cards(card_id):
+
+    card = Card.query.get(card_id)
+
+    if card is None:
+        return "", 404
+
+    db.session.delete(card)
+    db.session.commit()
+
+    return jsonify({
+        "details": f'Card {card.card_id} "{card.title}" successfully deleted.'
+    }) 

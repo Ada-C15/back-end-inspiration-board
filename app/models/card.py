5bb6d1ac69cead03a520ec0d76a1064a2044c0b8
Daniela Sanchez <dani.sanchez077@gmail.com>
2021-07-01 15:17:37 -0700

    updated card model

--- app/models/card.py
+++ app/models/card.py
@@ -6,7 +6,7 @@ class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String)
     likes_count = db.Column(db.Integer)
-    color = db.Column(db.String, default=None)
+    color = db.Column(db.String, default="yellow")
     board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'))
 
     def to_dict(self):

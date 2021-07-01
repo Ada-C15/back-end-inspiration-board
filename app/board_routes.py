ab4c5b41c2799c5ca5fb9a7433551485d82250b6
Araceli <aracelimaldonado4@yahoo.com>
2021-06-30 20:33:52 -0700

    updates to board routes, added a route to get only 1 board, the likes function semi-works, just doesn't increment yet but passes through postman

--- app/board_routes.py
+++ app/board_routes.py
@@ -4,8 +4,6 @@ from app import db
 from app.models.card import Card
 from app.models.board import Board
 
-
-
 boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
 
 # GET /boards
@@ -17,6 +15,15 @@ def get_boards():
         boards_list.append(board.board_to_json())
     return jsonify(boards_list), 200
 
+# GET 1 single board /boards
+@boards_bp.route("/<board_id>", methods=["GET"], strict_slashes=False)
+def get_one_board(board_id):
+    board = Board.query.get(board_id)
+    
+    if board is None:
+        return jsonify({"Error": f"Board {board_id} does not exist."}), 404
+    else:
+        return jsonify(board.board_to_json()), 200
 
 # POST /boards
 @boards_bp.route("", methods=["POST"], strict_slashes=False)
@@ -44,19 +51,30 @@ def get_cards(board_id):
 # POST /boards/<board_id>/cards
 @boards_bp.route("<board_id>/cards", methods=["POST"], strict_slashes=False)
 def add_card(board_id):
+
     current_board = Board.query.get(board_id)
     response = request.get_json() 
 
-    new_card = Card(message=response["message"],
-                likes_count=response["likes_count"],
+    new_card = Card(
+                message=response["message"],
+
+                # should the likes_count have a default value of 0?
+                # I added a default value of 0 to the card model on line 11
+                # I commented out line 64 and the post request works with just the "message" in the post request 
+                # likes_count=likes_count,
                 board=board_id)
+    
     db.session.add(new_card)
 
     current_board.cards.append(new_card)
     db.session.add(current_board)
     db.session.commit()
 
-    return jsonify(current_board.cards_list_to_json()), 201
+    # not sure if this is a preference, but line 74 returns a list 
+    # should we return just the 1 card each post request? 
+    # return jsonify(current_board.cards_list_to_json()), 201
+
+    return jsonify(new_card.card_to_json()), 201
 
 
 @boards_bp.route("<board_id>/cards/<card_id>", methods=["DELETE"], strict_slashes=False)
@@ -66,14 +84,19 @@ def delete_card(board_id, card_id):
     db.session.commit()
     return "deleted!", 200
 
+
 @boards_bp.route("<board_id>/cards/<card_id>/likes", methods=["PUT"], strict_slashes=False)
-def increase_likes(board_id) 
+def increase_likes(board_id, card_id): 
     response = request.get_json() 
-    current_board = Board.query.get(board_id)
-    card_id = response["card_id"]
-    card_in_this_board = current_board.cards[card_id] 
-    if card_in_this_board:
-        card_in_this_board.likes_count += 1
+    # current_board = Board.query.get(board_id)
+    
+    current_card = Card.query.get(card_id)
+
+
+
+    if board_id == current_card.board:
+        current_card.likes_count += 1
         db.session.commit()
     db.session.commit()
-    return "successssss", 200
+
+    return jsonify(current_card.card_to_json()), 200
--- app/models/card.py
+++ app/models/card.py
@@ -8,7 +8,7 @@ class Card(db.Model):
     __tablename__ = "cards"
     card_id = db.Column(db.Integer, primary_key=True) #, autoincrement=True)
     message = db.Column(db.String) # , nullable=False)
-    likes_count = db.Column(db.Integer) # , nullable=False)
+    likes_count = db.Column(db.Integer, default=0) # , nullable=False)
     board = db.Column(db.Integer, db.ForeignKey('boards.board_id')) # , nullable=False) 
     # had to remove nullable constraints
 
